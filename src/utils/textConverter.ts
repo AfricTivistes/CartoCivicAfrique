6d65c87976c5bddacdc04ab023f2363f55335da2
dofbi.eth <genova@dofbi.com>
2024-04-18 15:23:06 +0000

    ✨ feat (utils):  add sort & text converter

---
+++ src/utils/textConverter.ts
@@ -0,0 +1,28 @@
+// plainify
+export const plainify = (content: string) => {
+  if (!content) return null;
+
+  const filterBrackets = content.replace(/<\/?[^>]+(>|$)/gm, "");
+  const filterSpaces = filterBrackets.replace(/[\r\n]\s*[\r\n]/gm, "");
+  const stripHTML = htmlEntityDecoder(filterSpaces);
+  return stripHTML;
+};
+
+// strip entities for plainify
+const htmlEntityDecoder = (htmlWithEntities: string): string => {
+    let entityList: { [key: string]: string } = {
+      "&nbsp;": " ",
+      "&lt;": "<",
+      "&gt;": ">",
+      "&amp;": "&",
+      "&quot;": '"',
+      "&#39;": "'",
+    };
+    let htmlWithoutEntities: string = htmlWithEntities.replace(
+      /(&amp;|&lt;|&gt;|&quot;|&#39;)/g,
+      (entity: string): string => {
+        return entityList[entity];
+      }
+    );
+    return htmlWithoutEntities;
+  };
