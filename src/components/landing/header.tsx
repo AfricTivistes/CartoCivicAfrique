94ab9d20b43e1b696e0fcbae0e0d93f7ac43b12d
mbayedione10 <mbayedione10@gmail.com>
2024-06-11 17:22:50 +0000

    ✨ feat (hero): change bg

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -10,7 +10,7 @@ export function HeroSectionTwo() {
     <ThemeProvider>
       <Navbar />
       <header className="h-screen min-h-screen w-screen bg-white">
-        <div className="relative h-screen flex align-center flex-col justify-center text-center bg-cover" style={{backgroundImage: `url('https://images.unsplash.com/photo-1626761191814-a9dc9efd085c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2664&q=80')`}}>
+        <div className="relative h-screen flex align-center flex-col justify-center text-center bg-cover" style={{backgroundImage: `url('https://images.unsplash.com/photo-1592530392525-9d8469678dac?q=80&w=3164&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`}}>
           <span className="absolute top-0 left-0 w-full h-full bg-center bg-cover bg-gradient-to-tl from-gray-900 to-slate-800"></span>
           <Typography
             variant="h1"
@@ -19,16 +19,10 @@ export function HeroSectionTwo() {
           >
             Cartographie Citoyenne <br /> des Élections en Afrique
           </Typography>
-          <Typography
-            variant="lead"
-            color="white"
-            className="mb-12 w-full opacity-80 md:w-10/12 lg:w-7/12 mx-auto px-28 z-10"
-          >
-            Découvrez et engagez-vous avec les initiatives d'observation citoyenne, les actions de la société civile et les données cruciales qui façonnent la démocratie sur notre continent. Ensemble, construisons l'avenir électoral de l'Afrique.
-          </Typography>
+          
           <div className="w-auto mx-auto">
             <div className="flex items-center">
-              <Button color="dark" size="lg" className="w-full z-10">
+              <Button color="yellow" size="lg" className="w-full z-10">
               Explorez
               </Button>
             </div>
